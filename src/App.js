b5e41dcc7738aa997ea8c78f1149159341879b9f
sesugh <shulugh@fierafoods.com>
2023-07-20 16:29:45 -0400

    Adding .env file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import React from "react";
+
+
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <div>
+      <button className="square">{process.env.NODE_ENV}</button>
+      <br></br><br></br><br></br><br></br><br></br>
+      <button className="square">{process.env.REACT_APP_API_URL}</button>
+    </div>
+
+  );
 }
